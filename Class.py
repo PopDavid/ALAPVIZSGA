fab6dbc11f4994cc5630f74725b7dac4edb6f50b
Pop Dávid <pop.david.tibor@students.jedlik.eu>
2024-04-09 09:35:50 +0200

    python 3. feladata elkezd

---
+++ Class.py
@@ -0,0 +1,4 @@
+class Xxxxxxxxxxxxx:
+    def __init__(self, sor: str) -> None:
+        data = sor.strip().split()
+        self.xxxxxxxxxxxx = data[0]
---
+++ harm.py
@@ -0,0 +1,33 @@
+from Class import Xxxxxxxxxxxxx
+
+with open('xxxxxxxxxxxxxxx.csv', 'r', encoding='utf-8') as f:
+    myList = [Xxxxxxxxxxxxx(sor) for sor in f]
+
+print(f'A listában lévő elemek száma: {len(myList)}')
+
+legnagyobb_elem = max(myList, key=lambda x: x.xxxxxxxxxxxx) # x. után az kell, amire szeretnél max-ot keresni pl: x.ar
+
+legnagyobb_elem = min(myList, key=lambda x: x.xxxxxxxxxxxx) # x. után az kell, amire szeretnél min-t keresni pl: x.ar
+
+xxxxxxxxx_elemek_osszeadasa = sum(i.xxxxxxxxxxxx for i in myList if xxxxxxxxxxxxx_feltetel(i.xxxxxxxxxxxx)) 
+# Ha nincs feltétel akkor töröld ki: if xxxxxxxxxxxxx_feltetel(i.xxxxxxxxxxxx)
+
+def xxxxxxxxxxxxx_feltetel(ar): # xxxxxxxxxxxxx helyére írd be a feladat számát, lehet, hogy le kell még másolni
+    if ar > 5:
+        return True
+    return False
+
+
+
+xxxxxxxx_felteteles_kereses_count = 0
+
+for i in myList:
+    if xxxxxxxxxxxxx_feltetel(i.xxxxxxxxxxxx):
+        xxxxxxxx_felteteles_kereses_count += 1
+
+myDict = {}
+for i in myList:
+    myDict[i.xxxxxxxxxxxx] = myDict.get(i.xxxxxxxxxxxx, 0) + 1 
+# xxxxxxxxxxxx helyére a classnak kell a tulajdonsága.
+# a get fogja a i.xxxxxxxxxxxx értéket, hozzáad 1-et, ha nincs akkor 0
+# 1 helyett lehet más is
